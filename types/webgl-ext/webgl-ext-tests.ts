5b55c6bb82cf103a616a18d404d2b38485db7456
Bowen Ni <bowen1897@gmail.com>
2018-12-18 13:53:03 -0800

    WIP

--- types/webgl-ext/webgl-ext-tests.ts
+++ types/webgl-ext/webgl-ext-tests.ts
@@ -1,23 +1,23 @@
-
-
 var canvas = document.createElement("canvas");
 var gl = canvas.getContext("webgl");
-var ext: any;
 var t: any;
 
-if (ext = gl.getExtension("EXT_color_buffer_half_float")) {
-  t = ext.FRAMEBUFFER_ATTACHMENT_COMPONENT_TYPE_EXT;
-  t = ext.fsdf;
+var ext1: EXT_color_buffer_half_float;
+if (ext1 = gl.getExtension("EXT_color_buffer_half_float")) {
+  t = ext1.FRAMEBUFFER_ATTACHMENT_COMPONENT_TYPE_EXT;
 }
 
-if (ext = gl.getExtension("WEBGL_compressed_texture_atc")) {
-	t = ext.COMPRESSED_RGB_ATC_WEBGL;
+var ext2: WEBGL_compressed_texture_atc;
+if (ext2 = gl.getExtension("WEBGL_compressed_texture_atc")) {
+	t = ext2.COMPRESSED_RGB_ATC_WEBGL;
 }
 
-if (ext = gl.getExtension("WEBGL_compressed_texture_etc1")) {
-	t = ext.COMPRESSED_RGB_ETC1_WEBGL;
+var ext3: WEBGL_compressed_texture_etc1;
+if (ext3 = gl.getExtension("WEBGL_compressed_texture_etc1")) {
+	t = ext3.COMPRESSED_RGB_ETC1_WEBGL;
 }
 
-if (ext = gl.getExtension("WEBGL_compressed_texture_pvrtc")) {
-	t = ext.COMPRESSED_RGB_PVRTC_4BPPV1_IMG;
+var ext4: WEBKIT_WEBGL_compressed_texture_pvrtc;
+if (ext4 = gl.getExtension("WEBGL_compressed_texture_pvrtc")) {
+	t = ext4.COMPRESSED_RGB_PVRTC_4BPPV1_IMG;
 }
